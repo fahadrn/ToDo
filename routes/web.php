79710bda0c905e28308acf496b2ac32037d3c454
Fahad Rahmani <rnfahad3@gmail.com>
2024-02-22 17:45:57 +0530

    second

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [TodoController::class , 'home']);
+Route::get('/', [TodoController::class , 'home'])->name('home');
 Route::get('/admin', function () {
 return view('admin');
 });
+Route::post('/store',[TodoController::class , 'store'])-> name('store');
 Route::get('/contact' ,function () {
     return view('welcome');
 } );   
